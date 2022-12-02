a6eb76db5e3af3ba8aa6d321088fcbded5a71307
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2022-12-02 09:44:27 +0000

    break

--- Manual_rps.py
+++ Manual_rps.py
@@ -3,29 +3,35 @@ import random # imports random module
 
 
 
-def get_computer_choice():  #gets the computer choice randomly 
+def get_computer_choice():  #
+    #"""function that gets the computer's choice randomly """
     computer_choice = random.choice (["rock", "paper", "scissors"])
     return computer_choice
 
 
-def get_user_choice(): # gets the users choices 
-    user_choice = input ("What's your choice?(rock, paper, scissors)")
+def get_user_choice():
+    #"""function that gets the user's choices"""
+    user_choice =  input ("What's your choice?(rock, paper, scissors)")
     user_choice = user_choice.lower()
     return user_choice
     
+    
 
 def get_winner(computer_choice, user_choice):
-    #while True:
-    if user_choice == computer_choice:
-            print ("It is a tie!")
-    elif user_choice == "rock" and computer_choice == "scissors":
+    computer_choice = get_computer_choice
+    user_choice = get_user_choice
+    print (computer_choice)
+    if user_choice == "rock" and computer_choice == "scissors":
         print ("You won!")
     elif user_choice == "paper" and computer_choice == "rock":
         print ("You won!")
+        ###break
     elif user_choice == "scissors" and computer_choice == "paper":
         print ("You won!")
+        #break
     else:
         print ("You lost")
+  
     #break
 
  #if get_user_choice == "paper":
