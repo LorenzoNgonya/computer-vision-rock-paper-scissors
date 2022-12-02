c692d712288f00dc0edaeb7c8486205bc6f02f30
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2022-12-02 04:49:15 +0000

    error handling

--- Manual_rps.py
+++ Manual_rps.py
@@ -1,17 +1,17 @@
 
 import random # imports random module 
 
-choices = ["rock", "paper", "scissors"]
+
 
 def get_computer_choice():  #gets the computer choice randomly 
-    computer_choice = random.choice (choices)
-    return computer_choice
+    computer_choice = random.choice (["rock", "paper", "scissors"])
+    return get_computer_choice
 
 
 def get_user_choice(): # gets the users choices 
     user_choice = input ("What's your choice?(rock, paper, scissors)")
     user_choice = user_choice.lower()
-    return user_choice
+    return get_user_choice
     
 
 def get_winner(get_computer_choice, get_user_choice):
@@ -21,25 +21,28 @@ def get_winner(get_computer_choice, get_user_choice):
                 print ("You won!")
         elif get_computer_choice == "paper":
                 print ("You lost")
-    
+                
+        
         if get_user_choice == "paper":
             if get_computer_choice == "rock":
                 print ("You won!")
         elif get_computer_choice == "scissors":
                 print ("You lost")
+                
         
         if get_user_choice == "scissors":
             if get_computer_choice == "paper":
                 print ("You won!")
         elif get_computer_choice == 'rock':
                 print ("You lost")
-        else:
-            if get_computer_choice == get_user_choice:
+        else:    
+            if get_user_choice == get_computer_choice:
                 print ("It is a tie!")
-                
+             
+            
 def play():
-    get_computer_choice()
-    get_user_choice()
-    get_winner()
+    get_computer_choice
+    get_user_choice
+    get_winner(get_user_choice, get_computer_choice)
 
-
+play()
