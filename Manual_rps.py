97857af9b89a6df675cba8ffd3113defe5142224
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2022-12-02 05:34:51 +0000

    checking

--- Manual_rps.py
+++ Manual_rps.py
@@ -5,44 +5,37 @@ import random # imports random module
 
 def get_computer_choice():  #gets the computer choice randomly 
     computer_choice = random.choice (["rock", "paper", "scissors"])
-    return get_computer_choice
+    return computer_choice
 
 
 def get_user_choice(): # gets the users choices 
     user_choice = input ("What's your choice?(rock, paper, scissors)")
     user_choice = user_choice.lower()
-    return get_user_choice
+    return user_choice
     
 
-def get_winner(get_computer_choice, get_user_choice):
-    while True:
-        if get_user_choice == "rock":
-            if get_computer_choice == "scissors":
-                print ("You won!")
-        elif get_computer_choice == "paper":
-                print ("You lost")
-                
+def get_winner(computer_choice, user_choice):
+    #while True:
+    if user_choice == computer_choice:
+            print ("It is a tie!")
+    elif user_choice == "rock" and computer_choice == "scissors":
+        print ("You won!")
+    elif user_choice == "paper" and computer_choice == "rock":
+        print ("You won!")
+    elif user_choice == "scissors" and computer_choice == "paper":
+        print ("You won!")
+    #else:
+        print ("You lost")
+    #break
+
         
-        if get_user_choice == "paper":
-            if get_computer_choice == "rock":
-                print ("You won!")
-        elif get_computer_choice == "scissors":
-                print ("You lost")
-                
-        
-        if get_user_choice == "scissors":
-            if get_computer_choice == "paper":
-                print ("You won!")
-        elif get_computer_choice == 'rock':
-                print ("You lost")
-        else:    
-            if get_user_choice == get_computer_choice:
-                print ("It is a tie!")
-             
+       
+
+            
             
 def play():
-    get_computer_choice
-    get_user_choice
-    get_winner(get_user_choice, get_computer_choice)
+    get_computer_choice()
+    get_user_choice()
+    get_winner(get_user_choice,get_computer_choice)
 
 play()
