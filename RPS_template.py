321ad80fa5aa1e860cb89e829ddf77c7ae4fb5f8
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2022-12-06 03:40:39 +0000

    updates code

--- RPS_template.py
+++ RPS_template.py
@@ -1,6 +1,8 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
